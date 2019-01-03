3c5d6bb05a697406e83d9e57d7f2b14ccb3e95a2
Serenade092 <serenade092@berkeley.edu>
2019-01-02 21:15:50 -0500

    Update US News.py
    Added type, gender, year, religion, setting, as well as endowment of the universities.

--- US News.py
+++ US News.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8','Pragma':'no-cache','Referer':'https://www.usnews.com','Cache-Control':'no-cache','Accept-Language':'en-us','User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Safari/605.1.15','Accept-Encoding':'br, gzip, deflate','Connection':'keep-alive'}
-links,names,ranks,tuition,enrollment,city,state = [],[],[],[],[],[],[]
+links,names,ranks,tuition,enrollment,city,state,tYpe,gender,year,religion,setting,endowment = [],[],[],[],[],[],[],[],[],[],[],[],[]
 def crawl(url):
     req = requests.get(url,headers=headers)
     html = req.content
@@ -25,7 +25,29 @@ def crawl(url):
 for x in range(1,31):
     url=("https://www.usnews.com/best-colleges/rankings/national-universities?_page="+str(x))
     crawl(url)
-big=[links,names,ranks,tuition,enrollment,city,state]
+for i in links:
+    req = requests.get(i,headers=headers)
+    html = req.content
+    soup = BeautifulSoup(html, 'lxml')
+    stats=[]
+    for link in soup.find_all("p","block-normal"):
+        t=link.get_text()
+        result=re.search(r'founded in (\S+)', t)
+        if result:
+            year.append(result.group(0)[11:15])
+        result=re.search((r'\b(?:public|private)\b'), t)
+        if result:
+            tYpe.append(result.group(0))
+    for link in soup.find_all("span","heading-small text-black text-tight block-flush display-block-for-large-up"):
+        t=link.get_text()
+        stats.append(t)
+    tYpe.append(stats[0][:stats[0].find(',')])
+    gender.append(stats[0][stats[0].find(',')+2:])
+    year.append(stats[1])
+    religion.append(stats[2])
+    setting.append(stats[4])
+    endowment.append(stats[5])     
+big=[links,names,ranks,tuition,enrollment,city,state,tYpe,gender,year,religion,setting,endowment]
 big=list(map(list, zip(*big)))
 with open('USNewsRankings.csv', 'w') as f:
     writer = csv.writer(f)
